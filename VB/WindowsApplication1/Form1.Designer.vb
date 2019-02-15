135a6246243fd20a8e5bf9e7efade43473ea7b79
DevExpressExampleBot <fakeEmail>
2019-02-16 01:35:07 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -1,90 +1,88 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace WindowsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace WindowsApplication1
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.myGridControl1 = New WindowsApplication1.MyGridControl()
-			Me.myGridView1 = New WindowsApplication1.MyGridView()
-			Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colTrademark = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' myGridControl1
-			' 
-			Me.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.myGridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.myGridControl1.MainView = Me.myGridView1
-			Me.myGridControl1.Name = "myGridControl1"
-			Me.myGridControl1.Size = New System.Drawing.Size(739, 511)
-			Me.myGridControl1.TabIndex = 0
-			Me.myGridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.myGridView1})
-			' 
-			' myGridView1
-			' 
-			Me.myGridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colTrademark})
-			Me.myGridView1.GridControl = Me.myGridControl1
-			Me.myGridView1.Name = "myGridView1"
-			' 
-			' colID
-			' 
-			Me.colID.Caption = "ID"
-			Me.colID.FieldName = "ID"
-			Me.colID.Name = "colID"
-			Me.colID.Visible = True
-			Me.colID.VisibleIndex = 0
-			' 
-			' colTrademark
-			' 
-			Me.colTrademark.Caption = "Trademark"
-			Me.colTrademark.FieldName = "Trademark"
-			Me.colTrademark.Name = "colTrademark"
-			Me.colTrademark.OptionsFilter.FilterPopupMode = DevExpress.XtraGrid.Columns.FilterPopupMode.CheckedList
-			Me.colTrademark.Visible = True
-			Me.colTrademark.VisibleIndex = 1
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(739, 511)
-			Me.Controls.Add(Me.myGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.myGridControl1 = New WindowsApplication1.MyGridControl()
+            Me.myGridView1 = New WindowsApplication1.MyGridView()
+            Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colTrademark = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' myGridControl1
+            ' 
+            Me.myGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.myGridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.myGridControl1.MainView = Me.myGridView1
+            Me.myGridControl1.Name = "myGridControl1"
+            Me.myGridControl1.Size = New System.Drawing.Size(739, 511)
+            Me.myGridControl1.TabIndex = 0
+            Me.myGridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.myGridView1})
+            ' 
+            ' myGridView1
+            ' 
+            Me.myGridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colTrademark})
+            Me.myGridView1.GridControl = Me.myGridControl1
+            Me.myGridView1.Name = "myGridView1"
+            ' 
+            ' colID
+            ' 
+            Me.colID.Caption = "ID"
+            Me.colID.FieldName = "ID"
+            Me.colID.Name = "colID"
+            Me.colID.Visible = True
+            Me.colID.VisibleIndex = 0
+            ' 
+            ' colTrademark
+            ' 
+            Me.colTrademark.Caption = "Trademark"
+            Me.colTrademark.FieldName = "Trademark"
+            Me.colTrademark.Name = "colTrademark"
+            Me.colTrademark.OptionsFilter.FilterPopupMode = DevExpress.XtraGrid.Columns.FilterPopupMode.CheckedList
+            Me.colTrademark.Visible = True
+            Me.colTrademark.VisibleIndex = 1
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(739, 511)
+            Me.Controls.Add(Me.myGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.myGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.myGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
-		Private myGridControl1 As MyGridControl
-		Private myGridView1 As MyGridView
-		Private colID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colTrademark As DevExpress.XtraGrid.Columns.GridColumn
-	End Class
+        #End Region
+        Private myGridControl1 As MyGridControl
+        Private myGridView1 As MyGridView
+        Private colID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colTrademark As DevExpress.XtraGrid.Columns.GridColumn
+    End Class
 End Namespace
 
--- VB/WindowsApplication1/my_grid_control.vb
+++ VB/WindowsApplication1/my_grid_control.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports DevExpress.XtraGrid
+﻿Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Registrator
 Imports DevExpress.XtraGrid.Views.Base
@@ -7,47 +6,50 @@ Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Columns
 
 Namespace WindowsApplication1
-	Public Class MyGridControl
-		Inherits GridControl
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
-			MyBase.RegisterAvailableViewsCore(collection)
-			collection.Add(New MyGridInfoRegistrator())
-		End Sub
-	End Class
+    Public Class MyGridControl
+        Inherits GridControl
 
-	Public Class MyGridView
-		Inherits GridView
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Public Sub New(ByVal control As GridControl)
-			MyBase.New(control)
-		End Sub
-		Protected Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "MyGridView"
-			End Get
-		End Property
-		Protected Overrides Function CreateCheckedFilterPopup(ByVal column As GridColumn, ByVal ownerControl As Control, ByVal creator As Object) As CheckedColumnFilterPopup
-			Return New MyCheckedColumnFilterPopup(Me, column, ownerControl, creator)
-		End Function
-	End Class
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
+            MyBase.RegisterAvailableViewsCore(collection)
+            collection.Add(New MyGridInfoRegistrator())
+        End Sub
+    End Class
 
-	Public Class MyGridInfoRegistrator
-		Inherits GridInfoRegistrator
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Public Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "MyGridView"
-			End Get
-		End Property
-		Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
-			Return New MyGridView(grid)
-		End Function
-	End Class
+    Public Class MyGridView
+        Inherits GridView
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Public Sub New(ByVal control As GridControl)
+            MyBase.New(control)
+        End Sub
+        Protected Overrides ReadOnly Property ViewName() As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
+        Protected Overrides Function CreateCheckedFilterPopup(ByVal column As GridColumn, ByVal ownerControl As Control, ByVal creator As Object) As CheckedColumnFilterPopup
+            Return New MyCheckedColumnFilterPopup(Me, column, ownerControl, creator)
+        End Function
+    End Class
+
+    Public Class MyGridInfoRegistrator
+        Inherits GridInfoRegistrator
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+        Public Overrides ReadOnly Property ViewName() As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
+        Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
+            Return New MyGridView(grid)
+        End Function
+    End Class
 End Namespace
